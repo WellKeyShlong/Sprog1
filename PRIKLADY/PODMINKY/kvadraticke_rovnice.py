77893f32987e00b0fc10c1966e659540cd411e78
WellKeyShlong <vaclav.holubar.07@seznam.cz>
2026-02-19 13:04:09 +0100

    popopser

--- PRIKLADY/PODMINKY/kvadraticke_rovnice.py
+++ PRIKLADY/PODMINKY/kvadraticke_rovnice.py
@@ -1,15 +1,16 @@
 import math
 
-#import cisel
-a = int(input("Zadejte koeficient a: "))
-b = int(input("Zadejte koeficient b: "))
-c = int(input("Zadejte koeficient c: "))
+def kvadraticka_rovnice(a, b, c):
+    diskriminant = float((b ** 2) - (4*a*c))
 
-diskriminant = float((b ** 2) - (4*a*c))
+    if diskriminant <0:
+        return None
+    else:
+        x1 = float((-b + math.sqrt(diskriminant)) / 2*a)
+        x2 = float((-b - math.sqrt(diskriminant)) / 2*a)
+        return x1, x2
 
-if diskriminant >=0:
-    kořen1 = float((-b + math.sqrt(diskriminant)) / 2*a)
-    kořen2 = float((-b - math.sqrt(diskriminant)) / 2*a)
-    print(f"Kořeny rovnice jsou x1 = {kořen1}, x2 = {kořen2}")
-else:
-    print("Rovnice v R nemá řešení!!")
+# Příklad použití
+print(kvadraticka_rovnice(1, -5, 6))  # Očekávaný výstup: (3.0, 2.0)
+print(kvadraticka_rovnice(1, -4, 4))  # Očekávaný výstup: (2.0, 2.0)
+print(kvadraticka_rovnice(1, 2, 5))   # Očekávaný výstup: None
---
+++ PRIKLADY/PODMINKY/typ_trojuhelniku.py
@@ -0,0 +1,14 @@
+def typ_trojuhelniku(a, b, c):
+    if a = b and b = c:
+    # Zde napiš svůj kód
+    # 1. Zkontroluj trojúhelníkovou nerovnost
+    # 2. Zkontroluj rovnostranný
+    # 3. Zkontroluj pravoúhlý (Pythagorova věta)
+    # 4. Zkontroluj rovnoramenný
+    # 5. Jinak je různostranný
+    pass
+
+# Příklad použití
+print(typ_trojuhelniku(5, 5, 5))  # Očekávaný výstup: 'rovnostranný'
+print(typ_trojuhelniku(3, 4, 5))  # Očekávaný výstup: 'pravoúhlý'
+print(typ_trojuhelniku(5, 5, 8))  # Očekávaný výstup: 'rovnoramenný'
